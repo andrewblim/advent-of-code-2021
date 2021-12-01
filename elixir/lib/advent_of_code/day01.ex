78ffe101715bc9e5f8cb9980afe7323f920ab96d
Andrew Lim <andrew.b.lim@gmail.com>
2021-12-01 01:22:18 -0500

    Add day01

---
+++ elixir/lib/advent_of_code/day01.ex
@@ -0,0 +1,22 @@
+defmodule Day01 do
+  def hello do
+    :world
+  end
+
+  def read_input(file) do
+    {:ok, file} = File.read(file)
+    file |> String.trim() |> String.split("\n") |> Enum.map(&String.to_integer/1)
+  end
+
+  def n_increases(depths) do
+    {n, _} = depths |> Enum.reduce({0, nil}, fn
+      x, {count, prev} when x == nil or x > prev -> {count + 1, x}
+      x, {count, _} -> {count, x}
+    end)
+    n
+  end
+
+  def n_increases(depths, window) do
+    depths |> Enum.chunk_every(window, 1) |> Enum.map(&Enum.sum/1) |> n_increases()
+  end
+end
