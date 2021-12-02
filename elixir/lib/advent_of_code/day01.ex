3652086c35936a220e658b0ad9252654d6be8b3c
Andrew Lim <andrew.b.lim@gmail.com>
2021-12-01 20:49:53 -0500

    Remove hello-world-type stuff that mix generated

--- elixir/lib/advent_of_code/day01.ex
+++ elixir/lib/advent_of_code/day01.ex
@@ -1,18 +1,17 @@
 defmodule Day01 do
-  def hello do
-    :world
-  end
-
   def read_input(file) do
     {:ok, file} = File.read(file)
     file |> String.trim() |> String.split("\n") |> Enum.map(&String.to_integer/1)
   end
 
   def n_increases(depths) do
-    {n, _} = depths |> Enum.reduce({0, nil}, fn
-      x, {count, prev} when x == nil or x > prev -> {count + 1, x}
-      x, {count, _} -> {count, x}
-    end)
+    {n, _} =
+      depths
+      |> Enum.reduce({0, nil}, fn
+        x, {count, prev} when x == nil or x > prev -> {count + 1, x}
+        x, {count, _} -> {count, x}
+      end)
+
     n
   end
 
