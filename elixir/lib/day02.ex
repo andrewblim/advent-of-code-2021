6c1281f1d99a10393357b37504b679225e446292
Andrew Lim <andrew.b.lim@gmail.com>
2021-12-02 07:03:04 -0500

    Add day02

---
+++ elixir/lib/day02.ex
@@ -0,0 +1,36 @@
+defmodule Day02 do
+  def read_input(file) do
+    {:ok, file} = File.read(file)
+    file |>
+      String.trim() |>
+      String.split("\n") |>
+      Enum.map(fn x ->
+        [dir, n] = String.split(x, " ")
+        {dir, String.to_integer(n)}
+      end)
+  end
+
+  def move({direction, n}, {x, z}) do
+    case direction do
+      "forward" -> {x + n, z}
+      "down" -> {x, z + n}
+      "up" -> {x, z - n}
+    end
+  end
+
+  def navigate(instructions, {x, z} \\ {0, 0}) do
+    Enum.reduce(instructions, {x, z}, &move/2)
+  end
+
+  def move2({direction, n}, {x, z, aim}) do
+    case direction do
+      "forward" -> {x + n, z + n * aim, aim}
+      "down" -> {x, z, aim + n}
+      "up" -> {x, z, aim - n}
+    end
+  end
+
+  def navigate2(instructions, {x, z, aim} \\ {0, 0, 0}) do
+    Enum.reduce(instructions, {x, z, aim}, &move2/2)
+  end
+end
---
+++ elixir/test/day02_test.exs
@@ -0,0 +1,42 @@
+defmodule Day02Test do
+  use ExUnit.Case
+  doctest Day02
+
+  test "move" do
+    assert Day02.move({"forward", 5}, {0, 0}) == {5, 0}
+    assert Day02.move({"down", 10}, {0, 0}) == {0, 10}
+    assert Day02.move({"up", 15}, {0, 0}) == {0, -15}
+  end
+
+  test "navigate" do
+    assert Day02.navigate(
+      [
+        {"forward", 5},
+        {"down", 5},
+        {"forward", 8},
+        {"up", 3},
+        {"down", 8},
+        {"forward", 2},
+      ]
+    ) == {15, 10}
+  end
+
+  test "move2" do
+    assert Day02.move2({"forward", 5}, {0, 0, 0}) == {5, 0, 0}
+    assert Day02.move2({"down", 5}, {0, 0, 0}) == {0, 0, 5}
+    assert Day02.move2({"forward", 8}, {0, 0, 5}) == {8, 40, 5}
+  end
+
+  test "navigate2" do
+    assert Day02.navigate2(
+      [
+        {"forward", 5},
+        {"down", 5},
+        {"forward", 8},
+        {"up", 3},
+        {"down", 8},
+        {"forward", 2},
+      ]
+    ) == {15, 60, 10}
+  end
+end
