07c1eb027d8f6d0111f2a105c0037058b2db6eab
Andrew Lim <andrew.b.lim@gmail.com>
2021-12-05 08:16:41 -0500

    Run mix format on day05

--- elixir/lib/day05.ex
+++ elixir/lib/day05.ex
@@ -1,8 +1,10 @@
 defmodule Day05 do
   def parse_line(str) do
-    [x1, y1, x2, y2] = str
+    [x1, y1, x2, y2] =
+      str
       |> String.split([",", "->"])
-      |> Enum.map(fn x -> x |> String.trim() |> String.to_integer end)
+      |> Enum.map(fn x -> x |> String.trim() |> String.to_integer() end)
+
     {{x1, y1}, {x2, y2}}
   end
 
@@ -20,15 +22,18 @@ defmodule Day05 do
   end
 
   def get_horiz_vert_touches(lines) do
-    Enum.reduce(lines, %{}, fn {{x1, y1}, {x2, y2}}, acc->
+    Enum.reduce(lines, %{}, fn {{x1, y1}, {x2, y2}}, acc ->
       cond do
         is_horiz?({{x1, y1}, {x2, y2}}) ->
           pts = Map.new(x1..x2, fn k -> {{k, y1}, 1} end)
           Map.merge(acc, pts, fn _k, v1, v2 -> v1 + v2 end)
+
         is_vert?({{x1, y1}, {x2, y2}}) ->
           pts = Map.new(y1..y2, fn k -> {{x1, k}, 1} end)
           Map.merge(acc, pts, fn _k, v1, v2 -> v1 + v2 end)
-        true -> acc
+
+        true ->
+          acc
       end
     end)
   end
@@ -38,15 +43,19 @@ defmodule Day05 do
   end
 
   def get_all_touches(lines) do
-    Enum.reduce(lines, %{}, fn {{x1, y1}, {x2, y2}}, acc->
-      pts = cond do
-        is_horiz?({{x1, y1}, {x2, y2}}) ->
-          Map.new(x1..x2, fn k -> {{k, y1}, 1} end)
-        is_vert?({{x1, y1}, {x2, y2}}) ->
-          Map.new(y1..y2, fn k -> {{x1, k}, 1} end)
-        true ->
-          Map.new(Enum.zip(x1..x2, y1..y2), fn k -> {k, 1} end)
-      end
+    Enum.reduce(lines, %{}, fn {{x1, y1}, {x2, y2}}, acc ->
+      pts =
+        cond do
+          is_horiz?({{x1, y1}, {x2, y2}}) ->
+            Map.new(x1..x2, fn k -> {{k, y1}, 1} end)
+
+          is_vert?({{x1, y1}, {x2, y2}}) ->
+            Map.new(y1..y2, fn k -> {{x1, k}, 1} end)
+
+          true ->
+            Map.new(Enum.zip(x1..x2, y1..y2), fn k -> {k, 1} end)
+        end
+
       Map.merge(acc, pts, fn _k, v1, v2 -> v1 + v2 end)
     end)
   end
