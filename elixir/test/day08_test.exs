7b3629ade7f3d1b4cf66f6cc9a173f7ff0d6a8cd
Andrew Lim <andrew.b.lim@gmail.com>
2021-12-08 19:53:37 -0500

    Add day08 unit tests

---
+++ elixir/test/day08_test.exs
@@ -0,0 +1,35 @@
+defmodule Day08Test do
+  use ExUnit.Case
+  doctest Day08
+
+  setup_all do
+    input_str = """
+    be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """
+
+    input = input_str
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&Day08.parse_line/1)
+    {:ok, %{input: input}}
+  end
+
+  test "count_easy_digits", %{input: input} do
+    assert Day08.count_easy_digits(input) == 26
+  end
+
+  test "compute_digits", %{input: input} do
+    assert Day08.compute_digits(input) == [
+      8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315
+    ]
+  end
+end
